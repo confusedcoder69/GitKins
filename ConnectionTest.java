f7baee657ff8a52e4ab6dbe9c6bf44a87cb6727f
Aayush . <khurafatiaayush@gmail.com>
2024-06-20 12:26:42 +0530

    Text Updated

--- ConnectionTest.java
+++ ConnectionTest.java
@@ -2,7 +2,7 @@ class Test
 {
     public static void main(String []args)
     {
-        System.out.println("GitHub and Jetkins Connection Test.");
-        
+        System.out.println("GitHub and Jetkins Connection Established.");
+        System.out.println("This is the change. :-) ");
     }
 };
